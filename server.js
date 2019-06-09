6c9540980835b10d35fbbf238c1ea7e05e0df5da
Lilian Guimarães <lilian.guimaraes.silva@gmail.com>
2019-06-09 14:52:31 -0300

    change url mongodatabase reprograma

--- server.js
+++ server.js
@@ -2,54 +2,55 @@ const express = require("express");
 const maravilhosas = require("./schema.js");
 const bodyParser = require('body-parser');
 
-const db =  require("./repository.js");
+const db = require("./repository.js");
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URL = "mongodb://localhost:27017/reprograma";
+const MONGO_URL = "mongodb://localhost:27017/admin";
 // padrao: mongodb://dominio:porta/database
 
 db.connect(MONGO_URL)
+console.log('connect to mongodb')
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get("/maravilhosas", (req, res) => {
-  maravilhosas.find((error, response) => {
-    // preciso tratar o erro, caso ocorra
-    if (error) {
-      // caso tenha algum erro
-      return res.status(500).send(error);
-    }
-    // caso contrário, envio o retorno
-    res.status(200).send(response);
-  });
+    maravilhosas.find((error, response) => {
+        // preciso tratar o erro, caso ocorra
+        if (error) {
+            // caso tenha algum erro
+            return res.status(500).send(error);
+        }
+        // caso contrário, envio o retorno
+        res.status(200).send(response);
+    });
 })
 
 app.get("/maravilhosas/:id", (req, res) => {
-  maravilhosas.findById(
-    req.params.id,
-    function (err, maravilhosa) {
-      if (err) return res.send(err);
+    maravilhosas.findById(
+        req.params.id,
+        function(err, maravilhosa) {
+            if (err) return res.send(err);
 
-      if(!maravilhosa) return res.status(404).send({});
-console.log('*** maravilhosa.id:', maravilhosa.id)
-      res.send(maravilhosa);
-    }
-  );
+            if (!maravilhosa) return res.status(404).send({});
+            console.log('*** maravilhosa.id:', maravilhosa.id)
+            res.send(maravilhosa);
+        }
+    );
 });
 
 app.post("/maravilhosas", (req, res) => {
-  const novaMaravilhosa = new maravilhosas({
-    nome: req.body.nome,
-    bio: req.body.bio,
-    avatar: req.body.avatar,
-  });
-  
-  novaMaravilhosa.save(err => {
-    if (err) return res.send(err);
-  
-    res.send(novaMaravilhosa);
-  });
+    const novaMaravilhosa = new maravilhosas({
+        nome: req.body.nome,
+        bio: req.body.bio,
+        avatar: req.body.avatar,
+    });
+
+    novaMaravilhosa.save(err => {
+        if (err) return res.send(err);
+
+        res.send(novaMaravilhosa);
+    });
 });
 
 // app.put("/maravilhosas/:id", (req, res) => {
@@ -65,33 +66,30 @@ app.post("/maravilhosas", (req, res) => {
 // })
 
 app.patch("/maravilhosas/:id", (req, res) => {
-  maravilhosas.findByIdAndUpdate(
-    req.params.id,
-    { $set: req.body },
-    { new: true },
-    function(error, maravilhosa) {
-      if (error) {
-        let errorCode = error.code
-        if(!error.code){
-          errorCode = 400 // Bad Request
-        }
-        return res.status(errorCode).send(error.message);
-      }
+    maravilhosas.findByIdAndUpdate(
+        req.params.id, { $set: req.body }, { new: true },
+        function(error, maravilhosa) {
+            if (error) {
+                let errorCode = error.code
+                if (!error.code) {
+                    errorCode = 400 // Bad Request
+                }
+                return res.status(errorCode).send(error.message);
+            }
 
-      res.send(maravilhosa);
-    }
-  );
+            res.send(maravilhosa);
+        }
+    );
 })
 
 app.delete("/maravilhosas/:id", (req, res) => {
-  maravilhosas.findByIdAndDelete(
-    req.params.id,
-    { $set: req.body },
-    function(err, maravilhosa) {
-      if (err) return res.status(err.code).send(err.message);
-      res.send(maravilhosa);
-    }
-  );
+    maravilhosas.findByIdAndDelete(
+        req.params.id, { $set: req.body },
+        function(err, maravilhosa) {
+            if (err) return res.status(err.code).send(err.message);
+            res.send(maravilhosa);
+        }
+    );
 });
 
 
